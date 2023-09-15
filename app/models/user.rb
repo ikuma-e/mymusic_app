05e08f135e49ea8f614a379c75be336843daa5ce
ikuma-e <afxdrfjp2@gmail.com>
2023-09-15 15:49:34 +0900

    ユーザー登録の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # ActiveHashとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :birth_place
+  belongs_to :sex
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
+
+  validates :nickname, presence: true
+  validates :sex_id, numericality: { other_than: 1 }
+  validates :birth_place_id, numericality: { other_than: 1 }
+  validates :part, presence: true
+  validates :profile_description, presence: true
+
 end
--- db/migrate/20230914100506_devise_create_users.rb
+++ db/migrate/20230914100506_devise_create_users.rb
@@ -6,7 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string  :nickname,             null: false
+      t.integer :sex_id,               null: false
+      t.integer :birth_place_id,       null: false
+      t.string  :part,                 null: false
+      t.text    :profile_description,  null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
